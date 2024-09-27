f1aeed7d77350a3471b22c4b5d9f968c5730afe9
Cédric Walter <cedric.walter@gmail.com>
2024-09-27 16:03:13 +0200

    first commit

---
+++ static/mindmaps/linux-compromised-server-checks/old/map.mm
@@ -0,0 +1,378 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#338800" CREATED="1271625517486" ID="ID_937136709" MODIFIED="1378904402348" TEXT="Linux Compromised Server Checks&#xd;&#xd; &#xa;&#xd; What bad guys can do and remedies&#xa;&#xd; www.waltercedric.com">
+<edge COLOR="#990000" STYLE="linear" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1363797839073" ID="ID_1260657414" MODIFIED="1366055976002" POSITION="right" TEXT="Hacker do">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1366054705014" ID="ID_1525873138" MODIFIED="1366055976003" TEXT="Turn your server in a spam robot">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1272724545049" ID="ID_111864159" MODIFIED="1366054684055" TEXT="Set up unlimited email forwarders">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724456003" ID="ID_1060154686" MODIFIED="1366054675786" TEXT="Change DNS MX Entry  ">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724494426" ID="ID_1660350248" MODIFIED="1366054696962" TEXT="Created unlimited email accounts">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724521146" ID="ID_1628442462" MODIFIED="1366054692560" TEXT="Enabled/disabled boxtrapper and spam assasin">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1366054743782" ID="ID_423722498" MODIFIED="1366055976005" TEXT="Steal your Data hoping to steal your identity">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1272724507065" ID="ID_1833693827" MODIFIED="1366054765643" TEXT="Access your webmail">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1366054770941" ID="ID_212196840" MODIFIED="1366055976006" TEXT="May Ask relatives for money ">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1272724847402" ID="ID_1974103990" MODIFIED="1366054969430" TEXT="Use username and password found on disk/database to guess any other service/website">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724589778" ID="ID_1043205803" MODIFIED="1366054679928" TEXT="Change the default address my emails go to">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724581594" ID="ID_1822532391" MODIFIED="1366055190004" TEXT="Set up auto responders (follow-up or welcome emails)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055267014" ID="ID_1609067580" MODIFIED="1366055976008" TEXT="Access SSH, upload public keys to allow future access">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724686826" ID="ID_444083125" MODIFIED="1366055426933">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Create,&#160;Use, Disable your GPG Keys
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1366054802377" ID="ID_1600721732" MODIFIED="1366055976009" TEXT="Steal YOUR resources">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1272724926121" ID="ID_385267835" MODIFIED="1366054995893" TEXT="Stealing bandwidth from you : Turn off hotlink protection">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055015419" ID="ID_1708935988" MODIFIED="1366055976010" TEXT="storage">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1272724614193" ID="ID_334167029" MODIFIED="1366054928698" TEXT="Set up remote mysql hosts">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724882033" ID="ID_1908118399" MODIFIED="1366054670320" TEXT="Set up new sub ftp accounts">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724870786" ID="ID_11009800" MODIFIED="1366054799955">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Set up a webdisk (FTP Connection as a mapped/mounted drive)
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1272724699121" ID="ID_676141344" MODIFIED="1366055288175">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add unlimited subdomains
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055310018" ID="ID_670135259" MODIFIED="1366055976018" TEXT="Add addon domains (addon domain allows a new domain name to link to a subdirectory of your account. The addon domain&#x2019;s files will be stored in a subdirectory of your public_html (Document Root) directory. Addon domains allow you to control multiple domains from a single account.)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1366054852357" ID="ID_1218399675" MODIFIED="1366055976020" TEXT="Just want to delete and make you upset">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1272724741834" ID="ID_964448693" MODIFIED="1366054918970" TEXT="Create, Delete, View, Manipulate ANY mysql/postgres/oracle database">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724720001" ID="ID_1772095866" MODIFIED="1366055976020" TEXT="Park domains">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724892393" ID="ID_401240191" MODIFIED="1366055107180">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      view some data like site statistics
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055108836" ID="ID_1350113068" MODIFIED="1366055976022" TEXT="Delete some important data and their backup">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724731650" ID="ID_1731245557" MODIFIED="1366055141526" TEXT="Edit your DNS Settings">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055226250" ID="ID_62794791" MODIFIED="1366055976023" TEXT="Mess up configuration">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1272724654842" ID="ID_133602192" MODIFIED="1366055976023" TEXT="Change my mime type list">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724629577" ID="ID_1014764319" MODIFIED="1366054665899">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Change apache handlers
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1272724637674" ID="ID_829651983" MODIFIED="1366054640373">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Set up new cron jobs
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1366056013108" ID="ID_541305748" MODIFIED="1366056023618" TEXT="Encrypt your disk (Extorsion)"/>
+</node>
+<node COLOR="#111111" CREATED="1366055377369" ID="ID_1560269029" MODIFIED="1366055976025" TEXT="Keep a door open for next visit">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1272724905122" ID="ID_321133420" MODIFIED="1366055399102" TEXT="Deny IP Addresses">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055399977" ID="ID_183831876" MODIFIED="1366055976026" TEXT="Remove IP Address from Deny list">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1366056034812" ID="ID_972415006" MODIFIED="1366056047505" TEXT="Use your server as man in the middle">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1366055454332" ID="ID_675338553" MODIFIED="1366055976027" TEXT="Virtualize your whole server (P2V)">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1271625529814" ID="ID_1625715181" MODIFIED="1366054529511" TEXT="server not in vmware">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1271625538214" ID="ID_705791891" MODIFIED="1363797997457" TEXT="lsattr /proc/1/exe&#xa;------------- /proc/1/exe">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1366056050625" ID="ID_1926963884" MODIFIED="1366056082546" TEXT="you have NO chance to detect this except by running benchmark tests"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221510032861" ID="ID_1668599310" MODIFIED="1366055976028" POSITION="left" TEXT="Alert">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node COLOR="#111111" CREATED="1221509972236" ID="ID_667809975" MODIFIED="1366054529543" TEXT="your hosting company">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221510019126" ID="ID_64566179" MODIFIED="1363797997395" TEXT="big companies">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221509983080" ID="ID_699513202" MODIFIED="1363797997393" TEXT="they have logs, tools, honey pot, traffic shaping">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221510009126" ID="ID_833461018" MODIFIED="1363797997393" TEXT="they know what to do">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node CREATED="1378903970029" ID="ID_1666573166" MODIFIED="1378903984592" TEXT="get control back of your server">
+<node COLOR="#111111" CREATED="1221510045767" ID="ID_762283802" MODIFIED="1366054529545" TEXT="on your own?">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221509966283" ID="ID_1557671096" MODIFIED="1363797997396" TEXT="seek a professional">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221510129533" ID="ID_1708665676" MODIFIED="1378903999152" POSITION="left" TEXT="while server online">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#111111" CREATED="1221510188470" ID="ID_1308267850" MODIFIED="1366054544069">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      List running processes
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221510200408" ID="ID_1239131412" MODIFIED="1366054529551">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      open TCP/UDP ports
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1366055642059" ID="ID_1440278551" MODIFIED="1366055976032" TEXT="netstat -nat">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#111111" CREATED="1366055648888" ID="ID_658966033" MODIFIED="1366055976033" TEXT="netstat -na -f inet">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#111111" CREATED="1366055653847" ID="ID_1004273696" MODIFIED="1366055976034" TEXT="netstat -na -f inet | grep LISTEN">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#111111" CREATED="1366055662543" ID="ID_709081736" MODIFIED="1366055976034" TEXT="netstat -at | grep LISTEN">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221510220533" ID="ID_1070162826" MODIFIED="1366054561493">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Search for program images which are deleted but still running in main memory
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221510252361" ID="ID_141494899" MODIFIED="1366054569346">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      queues of connection requests
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221510270345" ID="ID_484119468" MODIFIED="1366054574578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Established connections
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221513391392" ID="ID_1556794202" MODIFIED="1363797997429" TEXT="nmap your server">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221513400892" ID="ID_1793084463" MODIFIED="1363797997430" TEXT="check for unkown ports">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221510298689" ID="ID_414020326" MODIFIED="1366054581553" TEXT="Kernel Modules loaded ">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1366055519428" ID="ID_1624253899" MODIFIED="1366055976038" TEXT="less /proc/modules">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#111111" CREATED="1366055527333" ID="ID_943617891" MODIFIED="1366055976038" TEXT="lsmod">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221509900142" ID="ID_410593153" MODIFIED="1378904014167" POSITION="left" TEXT="stop the server (offline)">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#111111" CREATED="1221509904080" ID="ID_1563331566" MODIFIED="1378904023315" TEXT="Bring your server offline by booting on rescue kernel">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221509913283" ID="ID_1674544887" MODIFIED="1366054601592" TEXT="mirror disk using dd">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1366055558256" ID="ID_1863425661" MODIFIED="1366055976040" TEXT="dd if=/dev/sda of=/dev/sdb bs=32M">
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221509933220" ID="ID_1990485810" MODIFIED="1366054529565" TEXT="boot a trusty operating system">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221509944080" ID="ID_64991504" MODIFIED="1363797997410" TEXT="knoppix">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221509947064" ID="ID_1529239633" MODIFIED="1363797997411" TEXT="optimized linux forensic distribution">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221510561080" ID="ID_199128710" LINK="http://penguinsleuth.org/" MODIFIED="1363797997407" TEXT="Penguin Sleuth Kit">
+<edge COLOR="#111111" WIDTH="thin"/>
+<arrowlink DESTINATION="ID_199128710" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1475144898" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_199128710" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1475144898" SOURCE="ID_199128710" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1221509920017" ID="ID_511601441" MODIFIED="1366054612472" TEXT="mount Old file system read only">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1221509956220" ID="ID_1616675882" MODIFIED="1366054529574" TEXT="analyse">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1221513414220" ID="ID_1243175385" MODIFIED="1363797997413" TEXT="check your logs!">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1366054433267" ID="ID_375509401" MODIFIED="1366055976044" POSITION="right" TEXT="Latest Version?">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1366054446616" ID="ID_273681839" LINK="http://linux-compromised-server-checks.waltercedric.com/&#xa0;" MODIFIED="1366054529589" TEXT="http://linux-compromised-server-checks.waltercedric.com/&#xa0;">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1366055754143" ID="ID_1769127666" LINK="http://www.waltercedric.com/" MODIFIED="1366055976046" TEXT="More? see http://www.waltercedric.com/">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1378904045472" ID="ID_1308214028" MODIFIED="1378904063352" POSITION="right" TEXT="Communities that fight badware">
+<node CREATED="1378904068977" ID="ID_227302303" LINK="http://www.badwarebusters.org" MODIFIED="1378904115578" TEXT="badwarebusters.org"/>
+<node CREATED="1378904076947" ID="ID_1918489714" LINK="http://www.stopbadware.org" MODIFIED="1378904119027" TEXT="stopbadware.org"/>
+<node CREATED="1378904084035" ID="ID_1275559518" LINK="http://www.antiphishing.org" MODIFIED="1378904122372" TEXT="antiphishing.org"/>
+<node CREATED="1378904092018" ID="ID_609984915" LINK="http://www.phishtank.com" MODIFIED="1378904125471" TEXT="phishtank.com"/>
+</node>
+<node CREATED="1378904138548" ID="ID_1725217362" MODIFIED="1378904145136" POSITION="right" TEXT="Ressources">
+<node CREATED="1378904154748" ID="ID_1241468673" LINK="http://googlewebmastercentral.blogspot.com/2008/04/my-sites-been-hacked-now-what.html" MODIFIED="1378904162198" TEXT="My sites have been hacked, now what?"/>
+<node CREATED="1378904166944" ID="ID_1893004438" LINK="http://googlewebmastercentral.blogspot.com/2007/09/quick-security-checklist-for-webmasters.html" MODIFIED="1378904174698" TEXT="Google&apos;s quick security checklist for webmasters"/>
+</node>
+</node>
+</map>
